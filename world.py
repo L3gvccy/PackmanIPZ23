a28e3c5c1ef976bf66ca8c4baadfc46695a2c3df
Arsen Altunyan <arsenaltunyan@knu.ua>
2025-03-02 15:08:54 +0200

    Update world add(update)

--- world.py
+++ world.py
@@ -101,3 +101,67 @@ class World:
 			self.player.sprite.direction = (0, 0)
 			self.player.sprite.status = "idle"
 			self.generate_new_level()
+
+
+	def update(self):
+		if not self.game_over:
+			# Рух гравця
+			pressed_key = pygame.key.get_pressed()
+			self.player.sprite.animate(pressed_key, self.walls_collide_list)
+
+			# Телепортування на протилежний бік карти
+			if self.player.sprite.rect.right <= 0:
+				self.player.sprite.rect.x = width
+			elif self.player.sprite.rect.left >= width:
+				self.player.sprite.rect.x = 0
+
+			# Поїдання ягід
+			for berry in self.berries.sprites():
+				if self.player.sprite.rect.colliderect(berry.rect):
+					if berry.power_up:
+						self.player.sprite.immune_time = 150
+						self.player.sprite.pac_score += 50
+					else:
+						self.player.sprite.pac_score += 10
+					berry.kill()
+
+			# Зіткнення з привидами
+			for ghost in self.ghosts.sprites():
+				if self.player.sprite.rect.colliderect(ghost.rect):
+					if not self.player.sprite.immune:
+						time.sleep(2)
+						self.player.sprite.life -= 1
+						self.reset_pos = True
+						break
+					else:
+						ghost.move_to_start_pos()
+						self.player.sprite.pac_score += 100
+
+		self._check_game_state()
+
+		# Відображення об'єктів на екрані
+		[wall.update(self.screen) for wall in self.walls.sprites()]
+		[berry.update(self.screen) for berry in self.berries.sprites()]
+		[ghost.update(self.walls_collide_list) for ghost in self.ghosts.sprites()]
+		self.ghosts.draw(self.screen)
+
+		self.player.update()
+		self.player.draw(self.screen)
+		self.display.game_over() if self.game_over else None
+
+		self._dashboard()
+
+		# Скидання позицій після зіткнення
+		if self.reset_pos and not self.game_over:
+			[ghost.move_to_start_pos() for ghost in self.ghosts.sprites()]
+			self.player.sprite.move_to_start_pos()
+			self.player.sprite.status = "idle"
+			self.player.sprite.direction = (0, 0)
+			self.reset_pos = False
+
+		# Перезапуск гри при натисканні кнопки "R"
+		if self.game_over:
+			pressed_key = pygame.key.get_pressed()
+			if pressed_key[pygame.K_r]:
+				self.game_over = False
+				self.restart_level()
