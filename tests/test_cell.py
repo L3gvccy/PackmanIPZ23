fb50dcaada2e057535aae039e5d486c3f298a1ae
L3gvccy <oleksandr_iv@knu.ua>
2025-03-12 21:57:02 +0200

    test cell

---
+++ tests/test_cell.py
@@ -0,0 +1,13 @@
+import pytest
+import pygame
+from cell import Cell  # Імпорт класу з cell.py
+
+@pytest.fixture
+def cell():
+    """Fixture для створення об'єкта клітинки."""
+    return Cell(0, 0, 32, 32, (0, 0, 0))  # Припустимо, що (0, 0) — це координати, а 50 — це розмір клітинки
+
+def test_cell_coordinates(cell):
+    """Тестуємо координати клітинки."""
+    assert cell.abs_x == 0
+    assert cell.abs_y == 0
